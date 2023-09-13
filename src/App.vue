710000aeed78c9a529998052e7a694a80fe33c18
danimog <daniele.moggia@gmail.com>
2023-09-13 23:44:46 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>🅅🄴🅁🄽🄰🅂🅂🄰.🄾🅁🄶</h1>
+    <h2>Dati ISTAT al 1 gennaio 2023..</h2>
+    <table>
+      <thead>
+        <th>Paese</th>
+        <th>Residenti</th>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Monterosso</td>
+          <td>1.350</td>
+        </tr>
+        <tr>
+          <td>Riomaggiore</td>
+          <td>1.326</td>
+        </tr>
+        <tr>
+          <td>Monterosso</td>
+          <td>729</td>
+        </tr>
+        <tr>
+          <td><b>TOTALE</b></td>
+          <td><b>3405</b></td>
+        </tr>
+      </tbody>
+    </table>
+
+    <h3>Turisti ipotizzati: 3 milioni</h3>
+    <h4>Ogni <strong>residente</strong> ha <em>"in carico"</em> 881 turisti</h4>
+
+  </div>
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+table{
+  width: 100%;
+}
+</style>
